ca1aff70bf32b574580099108530b508b9db338d
Emberhand <haydbill@gmail.com>
2024-07-13 20:14:03 -0600

    Session 2

--- scripts/place.js
+++ scripts/place.js
@@ -5,7 +5,9 @@ const lastModified = document.getElementById("lastModified")
 lastModified.innerText = new Date(document.lastModified);
 
 function calculateWindChill(temp, windSpeed) {
+    if (temp <= 10 && windSpeed > 4.8) {
     return Math.round(35.74 + (0.6215 * (temp)) - (35.75 * (windSpeed ** 0.16)) + (0.4275 * (temp) * (windSpeed ** 0.16)));
+    }else { return "N/A"}
 }
 
 const windchill = document.getElementById("windchill")
