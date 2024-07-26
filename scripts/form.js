8257f59234297ecddb387b117a0a083c1ff5f09e
Emberhand <haydbill@gmail.com>
2024-07-25 18:25:42 -0600

    Session 2

--- scripts/form.js
+++ scripts/form.js
@@ -6,49 +6,37 @@ lastModified.innerText = new Date(document.lastModified);
 
 const products = [
     {
-      id: fc-1888,
-      name: "flux capacitor",
-      averagerating: 4.5
+        id: fc - 1888,
+        name: "flux capacitor",
+        averagerating: 4.5
     },
     {
-      id: fc-2050,
-      name: "power laces",
-      averagerating: 4.7
+        id: fc - 2050,
+        name: "power laces",
+        averagerating: 4.7
     },
     {
-      id: fs-1987,
-      name: "time circuits",
-      averagerating: 3.5
+        id: fs - 1987,
+        name: "time circuits",
+        averagerating: 3.5
     },
     {
-      id: ac-2000,
-      name: "low voltage reactor",
-      averagerating: 3.9
+        id: ac - 2000,
+        name: "low voltage reactor",
+        averagerating: 3.9
     },
     {
-      id: jj-1969,
-      name: "warp equalizer",
-      averagerating: 5.0
+        id: jj - 1969,
+        name: "warp equalizer",
+        averagerating: 5.0
     }
-  ];
+];
 
-  function createInfoBox(selectedPage) {
-	document.querySelector(".res-grid").innerHTML = "";
-	selectedPage.forEach(page => {
-		let title = document.createElement("h2");
+const dropbox = document.getElementById("products");
 
-        card.setAttribute("class", "card")
-		title.textContent = page.title;
-        img.setAttribute("src", page.image);
-		img.setAttribute("alt", `${page.title}`);
-		img.setAttribute("loading", "lazy");
-		text.textContent = page.text;
-		button.innerHTML = `<div class="button">
-                        <button onClick="window.location.reload();">Return</button>
-                    </div>`;
-
-
-		card.appendChild(title);
-		card.appendChild(img);
-		card.appendChild(text);
-		card.appendChild(button);
+for (var i = 0; i < products.length; i++) {
+    var option = document.createElement('option');
+    option.setAttribute("value", products[i].id);
+    option.setAttribute("name", `${products[i].name}`);
+    dropbox.appendChild(option);
+}
